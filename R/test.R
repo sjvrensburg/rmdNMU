c3de635bd4ca9cda6b7d52784227c2abe5476d71
sjvrensburg <stefanj@mandela.ac.za>
2025-02-04 11:43:51 +0200

    Nicely styled test template.

--- R/test.R
+++ R/test.R
@@ -7,6 +7,7 @@
 #' @return R Markdown output format to pass to render
 #' @export
 tests <- function(..., keep_tex = TRUE) {
+  # Get template and instructions paths
   template <- system.file(
     "rmarkdown", "templates", "test", "resources",
     "template.tex", package = "rmdNMU")
@@ -15,28 +16,42 @@ tests <- function(..., keep_tex = TRUE) {
     "rmarkdown", "templates", "test", "resources",
     "instructions.tex", package = "rmdNMU")
 
-  base <- inherit_pdf_document(
+  # Get the font directory path and escape spaces/special chars
+  font_dir <- gsub("\\", "/",
+                   system.file("fonts", "nunito-sans", package = "rmdNMU"),
+                   fixed = TRUE)
+
+  # Verify font files exist
+  font_files <- c(
+    "NunitoSans-Regular.ttf",
+    "NunitoSans-Italic.ttf"
+  )
+
+  missing_fonts <- !sapply(font_files, function(f)
+    file.exists(file.path(font_dir, f)))
+  if (any(missing_fonts)) {
+    warning("Missing font files: ",
+            paste(font_files[missing_fonts], collapse = ", "))
+  }
+
+  base <- rmarkdown::pdf_document(
     ...,
     template = template,
     keep_tex = keep_tex,
+    latex_engine = "lualatex",
     includes = list(
-      in_header = NULL  # Add any header files if needed
+      in_header = NULL
     ),
     pandoc_args = c(
+      "--variable", paste0("fontdir=", font_dir),
       "--variable", paste0("instructions=", instructions)
     )
   )
 
+  base$inherits <- "pdf_document"
   base$knitr$opts_chunk$prompt <- FALSE
   base$knitr$opts_chunk$comment <- '# '
   base$knitr$opts_chunk$highlight <- TRUE
 
   return(base)
-}
-
-# Helper function to inherit from pdf_document
-inherit_pdf_document <- function(...) {
-  fmt <- rmarkdown::pdf_document(highlight = "monochrome", ...)
-  fmt$inherits <- "pdf_document"
-  return(fmt)
 }
