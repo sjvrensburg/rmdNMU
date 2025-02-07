e3cc5e6979679deced9e282244617b7168b19d48
sjvrensburg <stefanj@mandela.ac.za>
2025-02-07 09:15:57 +0200

    Fixed issue with code highlighting.

--- R/prac.R
+++ R/prac.R
@@ -1,10 +1,12 @@
 #' Render HTML Document With NMU Format
 #'
 #' @param toc Logical, include table of contents. Default is FALSE.
+#' @param highlight String or NULL that specifies the code highlighting style.
+#' @param ... Ignored
 #'
 #' @return R Markdown output format to pass to render.
 #' @export
-html_prac <- function(toc = FALSE) {
+html_prac <- function(toc = FALSE, highlight = "tango", ...) {
   css <- system.file("rmarkdown/templates/prac_or_lab/resources/template.css",
                      package = "rmdNMU")
 
@@ -12,7 +14,7 @@ html_prac <- function(toc = FALSE) {
   rmarkdown::html_document(
     fig_width = 6.5,
     fig_height = 4,
-    highlight = 'tango',
+    highlight = highlight,
     theme = NULL, # Remove default theme to use our custom CSS fully
     css = css,
     self_contained = TRUE,
